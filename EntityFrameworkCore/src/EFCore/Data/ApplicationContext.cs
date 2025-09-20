c4874a01ec07fd1f77cc0c6e70ec2ee0952964c1
Jorge Medina <medidrones@gmail.com>
2025-09-20 18:55:18 -0300

    Setup do Ambiente

---
+++ EntityFrameworkCore/src/EFCore/Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using EFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace EFCore.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EFCore;Integrated Security=True;Pooling=true";
+
+            optionsBuilder
+                .UseSqlServer(strConnection)
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+    }
+}
